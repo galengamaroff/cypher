478dc0e5a24bb1019667c8728c715b74a6a78d8e
CyberCircut1 <52717106+CyberCircut1@users.noreply.github.com>
2020-06-15 20:31:53 +0200

    Updated Cipher
    Updated the Cipher and Recipher

--- Recipher.js
+++ Recipher.js
@@ -1,73 +1,145 @@
 var inquirer = require('inquirer');
 const dictionary = require('./dictionary');
 const dictionaryIN = require('./dictionaryIN');
-
+//function cipher is the start of the program and calls the other functions.
 function cipher() {
-    inquirer
-        .prompt([{
-            type: 'list',
-            name: 'theme',
-            message: 'What do you want to do?',
-            choices: [
-                'Cipher a Sentence',
-                'End the program',
-                'Decript a code'
-            ]
-        }])
+
+    startQuestion().then(result => {
+
+        if (result === 'End the program') {
+            process.exit();
+        }
+
+        if (result === 'Decript a code') {
+
+            decryptCode().then(result => {
+                console.log('Result is', result)
+                cipher();
+            }, error => {
+                console.error('Error', error);
+                cipher();
+            })
+
+        }
+
+        if (result === 'Cipher a Sentence') {
+
+            cypherSentence().then(result => {
+                console.log('The result is: ', result)
+                cipher();
+            }, error => {
+                console.error('Error', error)
+                cipher();
+            })
+
+        }
+
+    }, error => {
+        cipher();
+    });
+
+}
+//starts asking questions
+function startQuestion() {
+
+    return new Promise((resolve, reject) => {
+
+        inquirer
+            .prompt([{
+                type: 'list',
+                name: 'theme',
+                message: 'What do you want to do?',
+                choices: [
+                    'Cipher a Sentence',
+                    'End the program',
+                    'Decript a code'
+                ]
+            }])
+            .then(answers => {
+
+                if (!answers.theme) {
+                    reject()
+                }
+
+                resolve(answers.theme);
+            });
+
+    })
+}
+//decrypts code
+function decryptCode() {
+
+    return new Promise((resolve, reject) => {
+
+        inquirer
+            .prompt([{
+                type: 'input',
+                name: 'message',
+                message: 'Write your sentence here. Please use _ as a space between characters.Remember not to use letters here.'
+            }])
+            .then(answers => {
+
+                if (!answers.message) {
+                    reject('No data received');
+                    return;
+                }
+
+                var decryptedMessage = '';
+
+                for (const index in answers.message) {
+                    var character = answers.message[index];
+                    var replacedCharacter = dictionaryIN[character];
+
+                    if (replacedCharacter) {
+                        decryptedMessage += replacedCharacter;
+                    }
+                }
+
+                resolve(decryptedMessage);
+            });
+
+
+    })
+
+}
+//cyphers sentences
+function cypherSentence() {
+
+    return new Promise((resolve, reject) => {
+
+        inquirer
+            .prompt([{
+                type: 'input',
+                name: 'message',
+                message: 'Write your sentence here. Please use _ as a space between words.'
+            }])
+
         .then(answers => {
-            if (answers.theme === 'End the program') {
-                process.exit();
+
+            if (!answers.message) {
+                reject('No message provided')
+                return;
             }
 
-            if (answers.theme === 'Decript a code') {
-                inquirer
-                    .prompt([{
-                        type: 'input',
-                        name: 'message',
-                        message: 'Write your sentence here. Please use _ as a space between characters.Remember not to use letters here.'
-                    }])
-                    .then(answers1 => {
-                        var decryptedMessage = '';
+            var encryptedMessage = '';
 
-                        for (const index in answers1.message) {
-                            var character = answers1.message[index];
-                            var replacedCharacter = dictionaryIN[character];
+            for (const index in answers.message) {
+                var character = answers.message[index];
+                character = character.toLowerCase();
+                var replacedCharacter = dictionary[character];
 
-                            if (replacedCharacter) {
-                                decryptedMessage += replacedCharacter;
-                            }
-                        }
-
-                        console.log(decryptedMessage);
-                        cipher();
-                    });
+                if (replacedCharacter) {
+                    encryptedMessage += replacedCharacter;
+                }
             }
 
-            if (answers.theme === 'Cipher a Sentence') {
-                inquirer
-                    .prompt([{
-                        type: 'input',
-                        name: 'message',
-                        message: 'Write your sentence here. Please use _ as a space between words.'
-                    }])
-                    .then(answers => {
-                        var encryptedMessage = '';
+            resolve(encryptedMessage);
 
-                        for (const index in answers.message) {
-                            var character = answers.message[index];
-                            character = character.toLowerCase();
-                            var replacedCharacter = dictionary[character];
-
-                            if (replacedCharacter) {
-                                encryptedMessage += replacedCharacter;
-                            }
-                        }
-                        console.log(encryptedMessage);
-
-                        cipher();
-                    });
-            }
         });
+
+    })
+
+
 }
 
 cipher();
